4f3a641c4edae83e815c056285e82cf0e1e23cbb
Vinit Keshri <vinitkeshri2020@gmail.com>
2024-07-11 14:34:20 +0530

    please deploy

--- frontend/notes-app/vite.config.js
+++ frontend/notes-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://vinitkesh.github.io/Notes3/",
+  base: "/Notes3",
   plugins: [react()],
 })
