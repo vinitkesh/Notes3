fc6368cd6d6a5309ddd1454c0a749144a848686e
Vinit Keshri <vinitkeshri2020@gmail.com>
2024-07-11 13:10:45 +0530

    deploy

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -9,7 +9,7 @@ import './App.css'
 import {BrowserRouter as Router , Routes, Route} from 'react-router-dom'
 
 const routes = (
-  <Router>
+  <Router basename={import.meta.env.DEV ? '/' : '/Notes3/'}>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<Home />} />
--- frontend/notes-app/src/main.jsx
+++ frontend/notes-app/src/main.jsx
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename="/Notes3">
-        <App />
-    </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
